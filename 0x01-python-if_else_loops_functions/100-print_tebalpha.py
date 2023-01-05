cceec9cde33971e62830796fe2bce65d88790fb4
mukarukuria <timothy.kuria@strathmore.edu>
2023-01-05 09:33:31 +0000

    Advanced tasks complete

---
+++ 0x01-python-if_else_loops_functions/100-print_tebalpha.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+for ch in range(ord('z'), ord('a') - 1, -1):
+    if ch % 2 == 0:
+        print(chr(ch), end="")
+    else:
+        print(chr(ch - 32), end="")
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+def remove_char_at(str, n):
+    result = ""
+    for i, ch in enumerate(str):
+        if i == n:
+            continue
+            
+        result += ch
+
+    return result
