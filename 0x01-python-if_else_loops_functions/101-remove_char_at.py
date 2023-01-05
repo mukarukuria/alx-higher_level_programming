43742daf35767181588653c4e9e299f5cf960247
mukarukuria <timothy.kuria@strathmore.edu>
2023-01-05 09:38:47 +0000

    modifications and corrections

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
 def remove_char_at(str, n):
@@ -6,7 +6,7 @@ def remove_char_at(str, n):
     for i, ch in enumerate(str):
         if i == n:
             continue
-            
+
         result += ch
 
     return result
