bc3a8db23fc4cc12b11d0c964902080663155279
mukarukuria <timothy.kuria@strathmore.edu>
2023-01-12 09:15:57 +0000

    Corrections

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     result = 0
-    if a_dictionary == None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     for i, el in enumerate(a_dictionary):
         if a_dictionary[el] > result:
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key =""):
+def simple_delete(a_dictionary, key=""):
     if key not in a_dictionary:
         return a_dictionary
     else:
