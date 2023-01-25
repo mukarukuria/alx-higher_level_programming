0d5711f759894a87366d399618194c9b526ea81b
mukarukuria <timothy.kuria@strathmore.edu>
2023-01-25 09:24:14 +0000

    New correction

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return (result)
-    except(TypeError):
+    except(ZeroDivisionError, IndexError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
