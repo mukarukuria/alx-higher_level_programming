d760f1dd947e0b92237c3d43ff95ea33ecaad61b
mukarukuria <timothy.kuria@strathmore.edu>
2023-01-25 08:57:39 +0000

    name change

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i, el in enumerate(my_list):
+            count += 1
+            if i == x:
+                break
+            else:
+                print("{}".format(el), end="")
+        print("")
+        return count
+    except(TypeError, ValueError):
+        print("Error occured for some reason")
