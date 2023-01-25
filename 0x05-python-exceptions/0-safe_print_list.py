3aeb7749a5647cb0f7e05cbd339cad023f9be9f5
mukarukuria <timothy.kuria@strathmore.edu>
2023-01-25 09:33:24 +0000

    new correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for i, el in enumerate(my_list):
-            count += 1
             if i == x:
                 break
             else:
                 print("{}".format(el), end="")
+            count += 1
         print("")
         return count
     except(TypeError, ValueError):
